95ea832898e3cd10ba9afd734d434f31f9221c92
Jackson <jacksonn3@7831c1b8f912.home>
2018-04-09 20:39:42 -0400

    Resetting the stupid Mac

--- Django/test-1-mac/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jacksonn3/Desktop/DBHI/Django/test-1-mac/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- Django/ticMACtoe/ticMACtoe/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def helloworld(request):
-	return HttpResponse("Hello world!")
-
-def home(request):
-	return HttpResponse("Welcome Home! Your request was: " + str(request))
